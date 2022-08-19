e1ec41d7aa57784391f4f29f6a9a69a6cb7aed15
Samuel <slopeza@uninorte.edu.co>
2022-08-19 17:35:15 -0500

    Miau

--- gif-expert-app/src/GifExpertApp.jsx
+++ gif-expert-app/src/GifExpertApp.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
 
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState([
+    "One Punch Man",
+    "Demon Slayer",
+    "Dragon Ball",
+  ]);
+
   return (
     <>
-        <h1>Hola mundo</h1>
+      <h1>Hola mundo</h1>
+      {categories.map((categoria, indice) => (
+        <p key={indice}>{categoria}</p>
+      ))}
     </>
-  )
-}
+  );
+};
